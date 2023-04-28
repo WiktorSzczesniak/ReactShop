e29fa7485aff183713503549a91f538632ce39c1
WiktorSzczesniak <wiktor.szcze00@gmail.com>
2023-04-28 04:21:13 +0200

    betterbutton

--- Shop/src/components/Body.tsx
+++ Shop/src/components/Body.tsx
@@ -1,34 +1,49 @@
 import "./Body.css";
 import Product from "./Product";
 import { useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 function Body() {
-  const divRef = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
   const handleScrollRight = () => {
-    if (divRef.current) {
-      divRef.current.scrollBy({
+    if (ref.current) {
+      ref.current.scrollBy({
         left: 650,
+        behavior: "smooth",
+      });
+    }
+  };
+  const handleScrollLeft = () => {
+    if (ref.current) {
+      ref.current.scrollBy({
+        left: -650,
         behavior: "smooth",
       });
     }
   };
   return (
     <div className="Body">
-      <ul className="list" ref={divRef}>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </ul>
-      <button className="scroll-button" onClick={handleScrollRight}>
-        Scroll Right
-      </button>
+      <div className="products">
+        <ul className="list" ref={ref}>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </ul>
+
+        <div className="scroll-button-right" onClick={handleScrollRight}>
+          <p> 〉</p>
+        </div>
+        <div className="scroll-button-left" onClick={handleScrollLeft}>
+          <p>〈</p>
+        </div>
+      </div>
     </div>
   );
 }
--- Shop/src/components/Header/header.tsx
+++ Shop/src/components/Header/header.tsx
@@ -4,14 +4,10 @@ import Upperheader from "./upperheader";
 import Bottomheader from "./bottomheader";
 import { useState, useEffect } from "react";
 function Header() {
- 
-
   return (
     <div className="mainHeader">
-
-        <Upperheader />
-        <Bottomheader />
-
+      <Upperheader />
+      <Bottomheader />
     </div>
   );
 }
